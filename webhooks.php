122c3b914dbbbbd0d7ff0a2d8cac827671d857e0
tunwadee narongirt <38591570+tunwadee@users.noreply.github.com>
2018-08-29 10:26:55 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'op9Un3BWWVYoLMtjSCAxT/vDkJVzA+ra/M6Blu2uMjgNQBJEMZWb/ESpPvx7h8SHpSyF+Wcp7ogOiuZ39/+RkncbFjLLKUQhE5LnBzK2usFTwDfOEd4LYDXL/1U7BsHyfoVuZkr6VRuRs9XWTgsezQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
